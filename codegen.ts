e1941c92d8284f2cbfa6d413d001153fc52a8968
RahinF <s3297352@student.rmit.edu.au>
2023-03-26 13:45:20 +1100

    added clsx

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: process.env.NEXT_PUBLIC_API_URL,
+  schema: process.env.NEXT_PUBLIC_API_URL!,
   ignoreNoDocuments: true,
   generates: {
     './src/gql/': {
